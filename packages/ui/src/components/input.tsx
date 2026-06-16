e0743a583eefee50249c19fa13c3b7e40c37695f
Gregory Bouteiller <gregory.bouteiller@gmail.com>
2026-06-17 03:04:50 +0400

    up

--- packages/ui/src/components/input.tsx
+++ packages/ui/src/components/input.tsx
@@ -3,14 +3,9 @@ import { type ComponentProps, splitProps } from "solid-js";
 import { cn } from "@/lib/utils";
 
 // STYLES ----------------------------------------------------------------------------------------------------------------------------------
-const INPUT =
-  cva(`z-input w-full h-9 min-w-0 outline-none rounded-md border border-input bg-white px-2.5 py-1 text-base shadow-xs outline-none
-  file:inline-flex file:border-0 file:bg-transparent file:text-foreground 
-  transition-[color,box-shadow] 
-  placeholder:text-muted-foreground 
-  focus-visible:border-ring focus-visible:ring-[3px] focus-visible:ring-ring/50 
-  disabled:pointer-events-none disabled:cursor-not-allowed disabled:opacity-50
-  aria-invalid:border-destructive aria-invalid:ring-[3px] aria-invalid:ring-destructive`);
+const INPUT = cva(
+  "z-input w-full min-w-0 outline-none file:inline-flex file:border-0 file:bg-transparent file:text-foreground placeholder:text-muted-foreground disabled:pointer-events-none disabled:cursor-not-allowed disabled:opacity-50"
+);
 
 // ROOT ------------------------------------------------------------------------------------------------------------------------------------
 export const Input = (props: InputProps) => {
