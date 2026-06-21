6e240ef8425b346bb22302e2ab3c8fc1c105614a
Gregory Bouteiller <gregory.bouteiller@niama.fr>
2026-06-21 17:42:45 +0400

    NIA-9: Remove contact modifications and add backend to solid2

--- apps/solid/src/functions/form.tsx
+++ apps/solid/src/functions/form.tsx
@@ -1,14 +1,13 @@
 import { api } from "@ec/backend/api";
 import { createConvexHttpClient } from "@ec/backend/client";
-import { submitContact, zContactCreateValues } from "@ec/domain/contacts";
+import { zContactCreateValues } from "@ec/domain/contacts";
 import { createServerFn } from "@tanstack/solid-start";
 
 // CONTACT ---------------------------------------------------------------------------------------------------------------------------------
 export const createContact = createServerFn({ method: "POST" })
-  .inputValidator(zContactCreateValues)
+  .validator(zContactCreateValues)
   .handler(async ({ data }) => {
     const convex = createConvexHttpClient(import.meta.env.VITE_CONVEX_URL);
-    await submitContact(data, async (contact) => {
-      await convex.mutation(api.contacts.create, contact);
-    });
+    await convex.mutation(api.contacts.create, data);
   });
+
