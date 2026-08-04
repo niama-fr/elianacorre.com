bb3d93401e3551df11547d69257ebea12773f135
Gregory Bouteiller <gregory.bouteiller@niama.fr>
2026-08-04 23:09:42 +0700

    NIA-45: Avoid anonymous cache bypass from form-state cleanup (#46)

--- apps/web/src/lib/form/form.functions.ts
+++ apps/web/src/lib/form/form.functions.ts
@@ -1,5 +1,7 @@
-import { getFormData } from "@tanstack/react-form-start";
 import { createServerFn } from "@tanstack/react-start";
 
-export const getServerFormState = createServerFn({ method: "GET" }).handler(async () => await getFormData());
-export type ServerFormState = Awaited<ReturnType<typeof getServerFormState>>;
+import { getFormState } from "./form.server";
+
+// GET SERVER FORM STATE -------------------------------------------------------------------------------------------------------------------
+export type { ServerFormState } from "./form.server";
+export const getServerFormState = createServerFn({ method: "GET" }).handler(async () => await getFormState());
